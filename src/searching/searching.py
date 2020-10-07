a4bfd06e5436398de724f52e36a1083396385c17
Erik Seguinte <ErikSeguinte@gmail.com>
2020-10-06 19:09:39 -0700

    iterative

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -9,11 +9,25 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    if not arr:
+        return -1
     # Your code here
-    return search_recursive(0, len(arr), arr, target)
-
-
-    return -1  # not found
+    left = 0
+    right = len(arr)
+    while left <= right:
+        if left == right:
+            if arr[left] == target:
+                return left
+            else:
+                return -1
+        middle = (left + right) // 2
+        value = arr[middle]
+        if value == target:
+            return middle
+        elif target < value:
+            right = middle
+        else:
+            left = middle + 1
 
 
 def search_recursive(left, right, arr,target):
